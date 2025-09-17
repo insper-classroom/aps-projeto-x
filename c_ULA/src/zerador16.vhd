9e57cf3a2faa6458e646c28c0e210180eb4b35f3
William Chang <williamwc@al.insper.edu.br>
2025-09-17 16:49:30 -0300

    zerador corrigido

--- c_ULA/src/zerador16.vhd
+++ c_ULA/src/zerador16.vhd
@@ -6,21 +6,22 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 entity zerador16 is
-  port(
+    port(
         z   : in STD_LOGIC;
-	      a   : in STD_LOGIC_VECTOR(15 downto 0);
+        a   : in STD_LOGIC_VECTOR(15 downto 0);
         y   : out STD_LOGIC_VECTOR(15 downto 0)
-      );
-end zerador16;
+    );
+end entity;
 
 architecture rtl of zerador16 is
-  -- Aqui declaramos sinais (fios auxiliares)
-  -- e componentes (outros módulos) que serao
-  -- utilizados nesse modulo.
+    -- Aqui declaramos sinais (fios auxiliares)
+    -- e componentes (outros módulos) que serao
+    -- utilizados nesse modulo.
 
 begin
-  with z select
-    y <= 0000000000000000 when '1',
-    a     when '0';
+    with z select
+        y <= "0000000000000000" when '1',
+             a                  when '0',
+             a                  when others;
 
 end architecture;

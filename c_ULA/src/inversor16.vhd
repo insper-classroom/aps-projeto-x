e4f8bbc55035e0617906792448bec68108ce57d8
William Chang <williamwc@al.insper.edu.br>
2025-09-17 16:44:31 -0300

    inversor corrigido

--- c_ULA/src/inversor16.vhd
+++ c_ULA/src/inversor16.vhd
@@ -6,22 +6,23 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 entity inversor16 is
-  port(
+    port(
         z   : in STD_LOGIC;
         a   : in STD_LOGIC_VECTOR(15 downto 0);
         y   : out STD_LOGIC_VECTOR(15 downto 0)
-      );
+    );
 end entity;
 
 architecture rtl of inversor16 is
-  -- Aqui declaramos sinais (fios auxiliares)
-  -- e componentes (outros módulos) que serao
-  -- utilizados nesse modulo.
+    -- Aqui declaramos sinais (fios auxiliares)
+    -- e componentes (outros módulos) que serao
+    -- utilizados nesse modulo.
 
 begin
 
-with z select
-  y <= not a when '1',
-       a     when '0';
+    with z select
+        y <= not a       when '1',
+             a           when '0',
+             a           when others;
 
 end architecture;

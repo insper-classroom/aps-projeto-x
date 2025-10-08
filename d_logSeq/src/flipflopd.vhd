24865edea4fb984e0cd654a285e5e3df5a79109e
GuForbes <80713991+GuForbes@users.noreply.github.com>
2025-10-08 15:36:11 -0300

    Update flipflopd.vhd

--- d_logSeq/src/flipflopd.vhd
+++ d_logSeq/src/flipflopd.vhd
@@ -8,7 +8,7 @@ use ieee.std_logic_1164.all;
 entity FlipFlopD is
 	port(
 		clock:  in std_logic;
-		d:      in std_logic;
+		D:      in std_logic;
 		clear:  in std_logic;
 		preset: in std_logic;
 		q:      out std_logic := '0'
@@ -23,11 +23,12 @@ begin
 	begin
 		if (clear = '1') then
 			Q <= '0';
+		elsif(preset = '1') then
+			Q <= '1';
 		elsif(rising_edge(clock)) then
 			Q <= D;
 		end if;
 
 	end process;
-
 
 end architecture;
